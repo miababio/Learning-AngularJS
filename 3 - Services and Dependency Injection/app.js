12bdd7e48946a9ddf24cc2bf6a2e245ea411db29
= <=>
2018-01-04 17:49:12 -0600

    Initial commit

---
+++ 3 - Services and Dependency Injection/app.js
@@ -0,0 +1,6 @@
+/* var myApp = angular.module("myApp", []);
+
+myApp.controller("mainController", function($scope, $log) {
+    $log.info($scope);
+}); */
+var myApp=angular.module("myApp",[]);myApp.controller("mainController",function(n,o){o.info(n)});
