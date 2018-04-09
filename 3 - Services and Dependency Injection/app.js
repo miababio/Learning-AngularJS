4623bea1c0b56d7bd5fe0c9e389b6701dbfd5622
= <=>
2018-04-09 13:21:23 -0500

    - Completed Section 3 - Modifed notes

--- 3 - Services and Dependency Injection/app.js
+++ 3 - Services and Dependency Injection/app.js
@@ -1,6 +1,7 @@
-/* var myApp = angular.module("myApp", []);
+var myApp = angular.module("myApp", []);
 
-myApp.controller("mainController", function($scope, $log) {
-    $log.info($scope);
-}); */
-var myApp=angular.module("myApp",[]);myApp.controller("mainController",function(n,o){o.info(n)});
+myApp.controller("mainController", ['$scope', '$log', function($scope, $log) {
+    console.log($scope);
+}]);
+// myApp.controller("mainController",["$scope","$log",function(o,n){n.info(o)}]);
+// var myApp=angular.module("myApp",[]);myApp.controller("mainController",function(n,o){o.info(n)});
